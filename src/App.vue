f5f74d313b8f120a8ab14d0ad43de9dcaf9390ab
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2019-01-10 05:56:34 -0500

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>CSS Specificity</h1>
+    <p>Not only is “CSS specificity” hard to say, but it can be difficult to understand. Use the calculator below to learn what your CSS selector’s specificity level is.</p>
+    <MainInput/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainInput from "./components/MainInput.vue";
 
 export default {
-  name: 'app',
+  //Name of the component used in main.js
+  name: "app",
   components: {
-    HelloWorld
+    MainInput
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "assets/styles/project";
 </style>
